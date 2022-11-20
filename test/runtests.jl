02f3509c95bed69b36119725a168bab00abc9bb0
mmcmanus1 <mattmcmanus41@gmail.com>
2022-11-20 18:11:53 -0500

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
-using v1
+using Aqua
+using Documenter
+using MyJuliaPackage
+using JuliaFormatter
 using Test
 
-@testset "v1.jl" begin
-    # Write your tests here.
+DocMeta.setdocmeta!(
+	MyJuliaPackage,
+	:DocTestSetup,
+	:(using MyJuliaPackage);
+	recursive=true
+)
+
+@testset verbose = true "MyJuliaPackage.jl" begin
+    @testset verbose = true "Code quality (Aqua.jl)" begin
+        Aqua.test_all(MyJuliaPackage; ambiguities=false)
+    end
+
+    @testset verbose = true "Code formatting (JuliaFormatter.jl)" begin
+        @test format(MyJuliaPackage; verbose=true, overwrite=false)
+    end
+
+    @testset verbose = true "Doctests (Documenter.jl)" begin
+        doctest(MyJuliaPackage)
+    end
+
+	@testset verbose = true "My own tests" begin
+		@test 1 + 1 == 2
+	end
 end
