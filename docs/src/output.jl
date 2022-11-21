3152a7bcb5e404479d044305668f7d78dd6d2ccd
mmcmanus1 <mattmcmanus41@gmail.com>
2022-11-21 15:08:23 -0500

    added to using

---
+++ docs/src/output.jl
@@ -0,0 +1,5 @@
+"""
+
+    output_file(solution)
+
+"""
--- docs/src/using.jl
+++ docs/src/using.jl
@@ -1,2 +1,23 @@
+"""
+
+    HashBlotto 
+
+A package that is to be used for class C25  
+"""
+
+
+
 module HashBlotto
 
+using HashCode2014
+
+export random_walking
+
+
+include("run_graph.jl")
+include("random.jl")
+include("functions.jl")
+include("output.jl")
+
+
+end
