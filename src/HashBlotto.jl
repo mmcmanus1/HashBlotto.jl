39f54e75b88b47ce6dfd6fcb75f8a4fed783fa8f
mmcmanus1 <mattmcmanus41@gmail.com>
2022-11-22 15:48:19 -0500

    fixed hashblotto code

--- src/HashBlotto.jl
+++ src/HashBlotto.jl
@@ -8,13 +8,14 @@ A package that is to be used for class C25
 
 
 module HashBlotto
+
 using HashCode2014
 export random_walking
+export greed
 
 
-include("run_graph.jl")
-include("random.jl")
-include("functions.jl")
+include("greed_test.jl")
+include("greedy_approach.jl")
 include("output.jl")
 
 
