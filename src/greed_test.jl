01830d6a1dbe8fadf95fdbe50131414d6c3881c0
paquito <hectorcan2000@gmail.com>
2022-11-22 23:25:35 -0500

    Better docstring

--- src/greed_test.jl
+++ src/greed_test.jl
@@ -1,15 +1,13 @@
 """
     greed(City)
 
-function that is greedy in it's approach to finding the best path,
-the logic it follows is looking at the distance/time ratio of the
-next junction and choosing the one with the highest ratio, if a car
-has already gone down one of the roads then we will calculate the reward
-based off of reward/ 2 * times it has gone down the road
+A greedy construction heuristic solution: Greedily maximize
+the distance/time ratio of the next junction. Reward is scaled down by 
+2 * repetitions of a street [if more than once] to discourage repeated edges
 ------------------------------------------------------------------------
-Building off of the HashCode2014 package (https://github.com/gdalle/HashCode2014.jl) that 
-was already built out for us, I definetly made it better as it is smarter in how
-it chooses paths than (https://github.com/gdalle/HashCode2014.jl/blob/main/src/random_walk.jl)
+We improve on the prior work of the HashCode2014 package (https://github.com/gdalle/HashCode2014.jl)
+by designing and implementing a greedy heuristic
+(https://github.com/gdalle/HashCode2014.jl/blob/main/src/random_walk.jl)
 """
 function greed(city)
     (; total_duration, nb_cars, starting_junction, streets) = city
