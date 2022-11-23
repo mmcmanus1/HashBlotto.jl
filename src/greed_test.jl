835fb567733240a493b3a3199a70d28df73902b1
Matt McManus <29646549+mmcmanus1@users.noreply.github.com>
2022-11-22 21:25:13 -0500

    Update greed_test.jl

--- src/greed_test.jl
+++ src/greed_test.jl
@@ -7,14 +7,10 @@ the logic it follows is looking at the distance/time ratio of the
 next junction and choosing the one with the highest ratio, if a car
 has already gone down one of the roads then we will calculate the reward
 based off of reward/ 2 * times it has gone down the road
-
-
 ------------------------------------------------------------------------
-
 Building off of the HashCode2014 package (https://github.com/gdalle/HashCode2014.jl) that 
 was already built out for us, I definetly made it better as it is smarter in how
-    it chooses paths than (https://github.com/gdalle/HashCode2014.jl/blob/main/src/random_walk.jl)
-
+it chooses paths than (https://github.com/gdalle/HashCode2014.jl/blob/main/src/random_walk.jl)
 """
 
 function greed(city)
