e89573ad0db3b1ef979e6e800d0a4092d3993140
mmcmanus1 <mattmcmanus41@gmail.com>
2022-11-28 14:31:40 -0500

    .gitignore is now working

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"index.html","page":"Home","title":"Home","text":"CurrentModule = HashBlotto","category":"page"},{"location":"index.html#HashBlotto","page":"Home","title":"HashBlotto","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"Documentation for HashBlotto.","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"Modules = [HashBlotto]","category":"page"}]
-}
